783fba6be583d08cfb246d6778a87582b4ca379c
AnjaliChathurani <108042163+AnjaliChathurani@users.noreply.github.com>
2023-05-30 21:46:59 +0530

    Update App.test.js

--- App.test.js
+++ App.test.js
@@ -1,18 +1,1 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App.js";
-import supertest from "supertest";
-import { response } from "express";
 
-describe("POST/api/products", () => {
-  test("should respond with a 200 status code", () => {
-    const respond = request(app).post("/api/products").send({
-      name: "name",
-      description: "description",
-      quantity: "quantity",
-      price: "price",
-      category: "category",
-    });
-    expect(response.statusCode).toBe(200);
-  });
-  describe("given a name,description,quantity,price and category", () => {});
-});
