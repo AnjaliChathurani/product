55151e74f138015b9846fe454b5c05b09aa6b81e
AnjaliChathurani <108042163+AnjaliChathurani@users.noreply.github.com>
2023-05-30 17:14:16 +0530

    Product_details.js

--- client/src/components/Product_details.js
+++ client/src/components/Product_details.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-
+// all product details 
 export default class Product_details extends Component {
   constructor(props) {
     super(props);
